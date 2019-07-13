bc8b52fea671861c1aa3483856588fbf5749a3da
cedric e goddard <8ceg6@comcast.net>
2019-07-13 23:03:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ var recipes={};
 function updateObjectWithKeyAndValue(object, key, value){
           return Object.assign({}, object, {[key]: value});
           }
-function updateObjectWithKeyAndValue(object, key, value){
-          return Object.assign({}, object, {[key]: value});
-          }
+
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
           object [key] =value;
